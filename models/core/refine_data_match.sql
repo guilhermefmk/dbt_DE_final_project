ca3fce8893938e827f5b44f14482801e08377613
Guilherme Cunha <guilhermefmk9@gmail.com>
2023-04-25 13:57:58 +0000

    done

---
+++ models/core/refine_data_match.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+SELECT
+  *,
+  CASE
+    WHEN row_num = 1 THEN 'nao'
+    ELSE 'sim'
+  END AS delete_case
+FROM {{ ref('row_numbers') }}
+where delete_case = 'nao'
